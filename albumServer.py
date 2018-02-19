cb12281e6139dd2f0d3a7de44de4181ac6a6353f
Max Ricketts-Uy <ricke1@umbc.edu>
2018-02-18 19:13:35 -0500

    sample pymongo program

---
+++ albumServer.py
@@ -0,0 +1,40 @@
+#ultimately server to interact with user to enter album reviews/data 
+#into mongo db
+from __future__ import print_function
+import pymongo
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+	con = MongoClient() #connection to db running on local machine
+	
+	db = con.albums #test_database is member var of con
+
+	albums = db.albums_data #tables = collections, creating people collection
+	#no schema, so any data can be entered as a record
+	#people.insert({'name':'Mike', 'food':'cheese'})
+	#people.insert({'name':'John', 'food':'ham'})
+	#people.insert({'name':"max", "gorp":"lorp"})
+
+	insert = True
+
+	while insert:
+		print("Enter album data? (Yes/No):", end='')
+		answer = raw_input()
+		if answer.lower() == "yes":
+			print("Enter album name: ", end='') 		
+			name = raw_input()
+			print("Enter artist: ", end='')
+			artist = raw_input()
+			print("Enter review: ", end='')
+			review = raw_input()
+			albums.insert({"name":name, "artists":artist, "review":review})
+		else:
+			insert = False
+	
+	#selects all data in collection and stores in list
+	album_data = albums.find() 
+
+	for shrimp in album_data:
+		print(shrimp)	
+	
+	db.albums_data.drop()	
